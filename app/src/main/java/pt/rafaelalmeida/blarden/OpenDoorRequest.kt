29cfa03c48452dad728d3156bf37d91f6112a0af
Rafael Almeida <rafaelcpalmeida@gmail.com>
2019-10-13 18:27:24 +0100

    Restore necessary code

--- app/src/main/java/pt/rafaelalmeida/blarden/OpenDoorRequest.kt
+++ app/src/main/java/pt/rafaelalmeida/blarden/OpenDoorRequest.kt
@@ -1,6 +1,8 @@
 package pt.rafaelalmeida.blarden
 
 import java.net.URL
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.net.HttpURLConnection
 
 
@@ -11,7 +13,14 @@ class OpenDoorRequest {
         val url = URL("$baseUrl/open-door")
 
         val conn = url.openConnection() as HttpURLConnection
+// 2. Open InputStream to connection
         conn.connect()
+        val `in` = conn.getInputStream()
+// 3. Download and decode the string response using builder
+        val stringBuilder = StringBuilder()
+        val reader = BufferedReader(InputStreamReader(`in`))
+        var line: String
+
 
         return true
     }
